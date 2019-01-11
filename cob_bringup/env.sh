a3003a1903a14b129c836cf44564384e783340a0
MattiaRacca <mattia.rh@gmail.com>
2019-01-11 14:07:34 +0200

    Revert "Merge branch 'indigo_dev' of https://github.com/ipa-cob4-8/cob_robots into indigo_dev"
    This reverts commit 00b785f9b771826134277fd3652434af114bba50, reversing
    changes made to 8882110d169ef9126a104a48881272278d5da5f0.

--- cob_bringup/env.sh
+++ cob_bringup/env.sh
@@ -3,20 +3,14 @@
 export MY_CATKIN_WORKSPACE=~/git/care-o-bot
 source /etc/cob.bash.bashrc
 
-if [ $(lsb_release -sc) == "trusty" ]; then
-    export MY_ROS_DISTRO="indigo"
-elif [ $(lsb_release -sc) == "xenial" ]; then
-    export MY_ROS_DISTRO="kinetic"
-fi
-
-. /opt/ros/$MY_ROS_DISTRO/setup.sh
+. /opt/ros/indigo/setup.sh
 
 if [ -e $MY_CATKIN_WORKSPACE/devel/setup.bash ]; then
     source $MY_CATKIN_WORKSPACE/devel/setup.bash
 elif [ -e /u/robot/git/care-o-bot/devel/setup.bash ]; then
     source /u/robot/git/care-o-bot/devel/setup.bash
 else
-    source /opt/ros/$MY_ROS_DISTRO/setup.bash
+    source /opt/ros/indigo/setup.bash
 fi
 
 export ROS_IP=`hostname -I | awk '{print $1}'`
